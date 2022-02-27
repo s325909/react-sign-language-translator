6c33948d4091d250b5990fca43d6cd9ffec85ecd
s325909 <jaso_cyril@hotmail.com>
2022-02-27 23:30:38 +0100

    FEAT: Clear user translations and update states and set user to updated user

--- src/components/Profile/ProfileActions.jsx
+++ src/components/Profile/ProfileActions.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { translationClearHistory } from "../../api/translation";
 import { STORAGE_KEY_USER } from "../../const/storageKeys";
 import { useUser } from "../../context/UserContext";
-import { storageDelete } from "../../utils/storage";
+import { storageDelete, storageSave } from "../../utils/storage";
 
 const ProfileActions = () => {
-	const { setUser } = useUser();
+	const { user, setUser } = useUser();
 
 	const handleSignOutClick = () => {
 		if (window.confirm("Are you sure?")) {
@@ -14,13 +15,34 @@ const ProfileActions = () => {
 		}
 	};
 
+	const handleClearHistoryClick = async () => {
+		if (!window.confirm("Are you sure? \nThis cannot be undone!")) {
+			return;
+		}
+
+		const [clearError] = await translationClearHistory(user.id);
+
+		if (clearError !== null) {
+			// Something went wrong...
+			return;
+		}
+
+		const updatedUser = {
+			...user,
+			translations: [],
+		};
+
+		storageSave(updatedUser);
+		setUser(updatedUser);
+	};
+
 	return (
 		<ul>
 			<li>
 				<Link to="/translation">Translations</Link>
 			</li>
 			<li>
-				<button>Clear history</button>
+				<button onClick={handleClearHistoryClick}>Clear history</button>
 			</li>
 			<li>
 				<button onClick={handleSignOutClick}>Sign out</button>
