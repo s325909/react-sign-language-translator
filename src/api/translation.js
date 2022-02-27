bc19ecbccd49961350c5eac2de672bc0b1120aff
s325909 <jaso_cyril@hotmail.com>
2022-02-27 23:19:25 +0100

    FEAT: Clear API user translations using PATCH

--- src/api/translation.js
+++ src/api/translation.js
@@ -23,4 +23,23 @@ export const translationAdd = async (user, translation) => {
 	}
 };
 
-export const translationClearHistory = (userId) => {};
+export const translationClearHistory = async (userId) => {
+	try {
+		const response = await fetch(`${apiURL}/${userId}`, {
+			method: "PATCH",
+			headers: createHeaders(),
+			body: JSON.stringify({
+				translations: [],
+			}),
+		});
+
+		if (!response.ok) {
+			throw new Error("Could not update the user translations.");
+		}
+
+		const result = await response.json();
+		return [null, result];
+	} catch (error) {
+		return [error.message, null];
+	}
+};
