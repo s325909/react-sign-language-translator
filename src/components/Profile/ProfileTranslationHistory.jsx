fa8b70e79b0bfbf0c1f66581394cbc1ab085175f
s325909 <jaso_cyril@hotmail.com>
2022-02-27 17:11:43 +0100

    FEAT: Added Translation History components to Profile.jsx

--- src/components/Profile/ProfileTranslationHistory.jsx
+++ src/components/Profile/ProfileTranslationHistory.jsx
@@ -1,4 +1,18 @@
-const ProfileTranslationHistory = () => {
-	return <p>Translation History</p>;
+import ProfileTranslationHistoryItem from "./ProfileTranslationHistoryItem";
+
+const ProfileTranslationHistory = ({ translations }) => {
+	const translationList = translations.map((translation, index) => (
+		<ProfileTranslationHistoryItem
+			key={index + " - " + translation}
+			translation={translation}
+		/>
+	));
+
+	return (
+		<section>
+			<h4>Your translation history:</h4>
+			<ul>{translationList}</ul>
+		</section>
+	);
 };
 export default ProfileTranslationHistory;
