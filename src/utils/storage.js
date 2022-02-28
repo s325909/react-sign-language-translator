7768e3231c24e02d1c50641e023cbbafd59a4d51
s325909 <jaso_cyril@hotmail.com>
2022-02-28 14:13:31 +0100

    FEAT: Validate storage key function

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,13 +1,17 @@
-export const storageSave = (key, value) => {
-	if (!key || typeof key !== "string") throw new Error("storageSave: Invalid storage key provided!");
-	if (!value) throw new Error("storageSave: No value provided for: " + key);
+const validateKey = (key, location) => {
+	if (!key || typeof key !== "string") {
+		throw new Error(location + "Invalid storage key provided!");
+	}
+};
 
+export const storageSave = (key, value) => {
+	validateKey(key, "storageSave: ");
+	if (!value) throw new Error("storageSave: No value provided for: " + key);
 	localStorage.setItem(key, JSON.stringify(value));
 };
 
 export const storageRead = (key) => {
-	if (!key || typeof key !== "string") throw new Error("storageRead: Invalid storage key provided!");
-
+	validateKey(key, "storageRead: ");
 	const data = localStorage.getItem(key);
 	if (data) return JSON.parse(data);
 
@@ -15,7 +19,6 @@ export const storageRead = (key) => {
 };
 
 export const storageDelete = (key) => {
-	if (!key || typeof key !== "string") throw new Error("storageDelete: Invalid storage key provided!");
-
+	validateKey(key, "storageDelete: ");
 	localStorage.removeItem(key);
 };
